22078180e1457203d77c9a765ec07b889dd351eb
Pallav <pallav.majumdar@snapdeal.com>
2015-10-08 13:30:42 +0530

    UI elements initiation

---
+++ UI/index.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="bootstrap/js/npm.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <h1>Dayum!! Server's up maaayn</h1>
+
+        	<div style="padding-left: 2%; padding-bottom: 15px"><button class="btn btn-info"> < HOME</button></div>
+
+        	<div>
+        		<nav class="navbar navbar-default">
+		        	<ul class="nav nav-tabs">
+		        		<li class="active"><a>BRD Requirements</a></li>
+		        		<li><a>Tech Dev Need</a></li>
+		        		<li><a>Content Need</a></li>
+		        		<li><a>Training & Communication Plan</a></li>
+		        		<li><a>Capabilities Enhancement</a></li>
+		        		<li><a>Cost Benefit</a></li>
+		        		<li><a>Risk Mitigation Plan</a></li>
+		        		<li><a>Go Live Plan</a></li>
+		        		<li><a>Closure</a></li>
+		        	</ul>
+	        	</nav>
+	        </div>
+        	<!-- <div style="margin-left: 5%">
+	        	<li class="active"><a>BRD Requirements</a></li>
+		        		<li><a>Tech Dev Need</a></li>
+		        		<li><a>Content Need</a></li>
+		        		<li><a>Training & Communication Plan</a></li>
+		        		<li><a>Capabilities Enhancement</a></li>
+		        		<li><a>Cost Benefit</a></li>
+		        		<li><a>Risk Mitigation Plan</a></li>
+		        		<li><a>Go Live Plan</a></li>
+		        		<li><a>Closure</a></li>
+			</div> -->
+        <div style="padding: 1% 5% 5% 5%; border: 25px" >
+
+	        <table class='table table-bordered'>
+	            <thead>
+			      <tr class="info">
+			        <th>Project</th>
+			        <th>Field1</th>
+			        <th>Field2</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			        <td>John</td>
+			        <td>Doe</td>
+			        <td>john@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>Mary</td>
+			        <td>Moe</td>
+			        <td>mary@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>July</td>
+			        <td>Dooley</td>
+			        <td>july@example.com</td>
+			      </tr>
+			    </tbody>
+	        </table>
+	    </div>
+    </body>
+</html>
+<?php
+    // echo "It works!!"
+?>
