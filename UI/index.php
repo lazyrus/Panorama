c2f6f6bd0ab884ba67eefa4876af44389749b8de
Pallav <pallav.majumdar@snapdeal.com>
2015-10-08 17:10:57 +0530

    mod

--- UI/index.php
+++ UI/index.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+    	<title>Panorama</title>
         <script src="bootstrap/js/bootstrap.min.js"></script>
         <script src="bootstrap/js/npm.js"></script>
 
@@ -7,9 +8,9 @@
     </head>
 
     <body>
-        <h1>Dayum!! Server's up maaayn</h1>
+        	<div style="margin: 2%"><button class="btn"> < HOME</button></div>
+        	<h1>Project Name</h1>
 
-        	<div style="padding-left: 2%; padding-bottom: 15px"><button class="btn btn-info"> < HOME</button></div>
 
         	<div>
         		<nav class="navbar navbar-default">
@@ -26,46 +27,87 @@
 		        	</ul>
 	        	</nav>
 	        </div>
-        	<!-- <div style="margin-left: 5%">
-	        	<li class="active"><a>BRD Requirements</a></li>
-		        		<li><a>Tech Dev Need</a></li>
-		        		<li><a>Content Need</a></li>
-		        		<li><a>Training & Communication Plan</a></li>
-		        		<li><a>Capabilities Enhancement</a></li>
-		        		<li><a>Cost Benefit</a></li>
-		        		<li><a>Risk Mitigation Plan</a></li>
-		        		<li><a>Go Live Plan</a></li>
-		        		<li><a>Closure</a></li>
-			</div> -->
-        <div style="padding: 1% 5% 5% 5%; border: 25px" >
 
-	        <table class='table table-bordered'>
+        <div style="margin: 0px">
+
+	        <table class='table table-bordered table-condensed'>
 	            <thead>
 			      <tr class="info">
-			        <th>Project</th>
-			        <th>Field1</th>
-			        <th>Field2</th>
+			        <th title="Line Items">Line Items</th>
+			        <th title="BRD Ref Number">BRD Ref Number</th>
+			        <th title="BRD Date">BRD Date</th>
+			        <th title="Approved">Approved</th>
+			        <th title="Approved by">Approved by</th>
+			        <th title="Approval date">Approval date</th>
+			        <th title="Stakeholders">Stakeholders</th>
+			        <th title="Stakeholder Biz Units">Stakeholder BU</th>
+			        <th title="Expected Impact Biz Units">Expected Impact BU</th>
+			        <th title="Stakeholders Approved">Stkhld App..</th>
+			        <th title="Stk App Date">Stk App Date</th>
+			        <th title="Responsible">Resp..</th>
+			        <th title="Accountable">Acc..</th>
+			        <th title="Consulted">Consulted</th>
+			        <th title="Informed">Infd..</th>
 			      </tr>
 			    </thead>
 			    <tbody>
 			      <tr>
-			        <td>John</td>
-			        <td>Doe</td>
-			        <td>john@example.com</td>
+			        <td class="warning">PHOENIX</td>
+			        <td>420</td>
+			        <td>12-June-2015</td>
+			        <td>Y</td>
+			        <td>Random Guy</td>
+			        <td>13-June</td>
+			        <td>BC, MC</td>
+			        <td>MD, BD</td>
+			        <td>SD</td>
+			        <td>Y</td>
+			        <td>21-Jun</td>
+			        <td>NaMo</td>
+			        <td>MaMo</td>
+			        <td>KaMo</td>
+			        <td>Komodo</td>
 			      </tr>
 			      <tr>
-			        <td>Mary</td>
-			        <td>Moe</td>
-			        <td>mary@example.com</td>
+			        <td class="warning">Stakeholder's Meet</td>
+			        <td>500</td>
+			        <td>14-June-2015</td>
+			        <td>Y</td>
+			        <td>Another Random Guy</td>
+			        <td>16-June</td>
+			        <td>BC, MC</td>
+			        <td>MD, BD</td>
+			        <td>SD</td>
+			        <td>Y</td>
+			        <td>20-Jun</td>
+			        <td>Bhak</td>
+			        <td>Jaa na be</td>
+			        <td>Kya be</td>
+			        <td>Dongo</td>
 			      </tr>
 			      <tr>
-			        <td>July</td>
-			        <td>Dooley</td>
-			        <td>july@example.com</td>
+			        <td class="warning">ORM Escalation Management</td>
+			        <td>32</td>
+			        <td>14-June-2015</td>
+			        <td>Y</td>
+			        <td>Random Guy again</td>
+			        <td>20-June</td>
+			        <td>MC, BC</td>
+			        <td>BD, MD</td>
+			        <td>SD</td>
+			        <td>Y</td>
+			        <td>21-Jun</td>
+			        <td>Al</td>
+			        <td>Lorem</td>
+			        <td>Ipsum</td>
+			        <td>Randum</td>
 			      </tr>
 			    </tbody>
 	        </table>
+
+	        <button class="btn btn-primary" style="margin-left: 48%">EDIT</button>
 	    </div>
+
     </body>
 </html>
 <?php
